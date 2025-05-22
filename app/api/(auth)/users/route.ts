5ad7b8e6d29b3d73d58da1bfe663bca10865aab2
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-05-22 14:59:59 +0100

    first api get request

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,5 +1,15 @@
 import { NextResponse } from "next/server";
+import User from "../../../../lib/modals/user";
+import connectDB from "@/lib/db";
 
-export const GET = () => {
-  return new NextResponse("This is my first API on Next.js");
+export const GET = async () => {
+  try {
+    await connectDB();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: any) {
+    return new NextResponse("Error fetching the user data" + error.message, {
+      status: 500,
+    });
+  }
 };
