141b3a5b507489d188a1fcd1fe52a0a564462620
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-05-22 18:39:18 +0100

    put nextjs api request

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,11 +1,16 @@
 import { NextResponse } from "next/server";
 import User from "../../../../lib/modals/user";
 import connectDB from "@/lib/db";
+import { Types } from "mongoose";
+
+const ObjectId = require("mongoose").Types.ObjectId;
 
 export const GET = async () => {
   try {
     await connectDB();
+
     const users = await User.find();
+
     return new NextResponse(JSON.stringify(users), { status: 200 });
   } catch (error: any) {
     return new NextResponse("Error fetching the user data" + error.message, {
@@ -17,9 +22,11 @@ export const GET = async () => {
 export const POST = async (request: Request) => {
   try {
     const body = await request.json();
+
     await connectDB();
 
     const newUser = new User(body);
+
     await newUser.save();
 
     return new NextResponse(
@@ -29,6 +36,57 @@ export const POST = async (request: Request) => {
   } catch (error: any) {
     console.log("Error :", error);
     return new NextResponse("Error in creating user" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const PUT = async (request: Request) => {
+  try {
+    const body = await request.json();
+
+    const { userId, newUsername } = body;
+
+    await connectDB();
+
+    if (!userId || !newUsername) {
+      return new NextResponse(
+        JSON.stringify({ message: "ID or new username not found" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User id" }), {
+        status: 400,
+      });
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { username: newUsername },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        { status: 400 }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "User is updated succesfully!",
+        user: updatedUser,
+      }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.log("Error :", error);
+    return new NextResponse("Error updating user" + error.message, {
       status: 500,
     });
   }
