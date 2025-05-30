37c81833400b7ff6c95c61ad0041c662f405db33
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-05-30 01:37:19 +0100

    blog get requiest made my the specific user with his category id category

---
+++ app/api/(dashboard)/blogs/route.ts
@@ -0,0 +1,71 @@
+import { NextResponse } from "next/server";
+import User from "../../../../lib/modals/user";
+import Category from "@/lib/modals/category";
+import connectDB from "@/lib/db";
+import { Types } from "mongoose";
+import Blog from "@/lib/modals/blog";
+
+export const GET = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const userId = searchParams.get("userId");
+
+    const categoryId = searchParams.get("categoryId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing CategoryId" }),
+        { status: 400 }
+      );
+    }
+
+    await connectDB();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 400,
+      });
+    }
+
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({ message: "Category not found" }),
+        { status: 400 }
+      );
+    }
+
+    const filter: any = {
+      user: new Types.ObjectId(userId),
+      category: new Types.ObjectId(categoryId),
+    };
+
+    // TODO
+
+    const blog = await Blog.find(filter);
+
+    return new NextResponse(JSON.stringify({ blog }), { status: 200 });
+  } catch (error: any) {
+    console.log("Error :", error);
+    return new NextResponse("Error getting blog " + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const POST = async (request: Request) => {
+  try {
+  } catch (error: any) {
+    console.log(first);
+  }
+};
